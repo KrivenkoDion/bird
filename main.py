fa7d23b2f2d369f22f4d5e68b0df32225d47dae6
KrivenkoDion <dion.krivenko@student.howest.be>
2026-03-16 17:33:54 +0100

    species endpoint working

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from database import start_db
+from routers.species import router as species_router
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    start_db()
+
+
+app.include_router(species_router)
+
 
 @app.get("/")
 async def root():
